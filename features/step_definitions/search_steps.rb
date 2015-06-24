a818eb25490b35cc16ac5e5622bb5e07ac79f6d6
Neil <neil@Neils-MacBook-Pro.local>
2015-06-24 00:02:54 -0700

    formatting

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -4,8 +4,7 @@ end
 
 When /I search for "(.*)"/ do |query|
   page = GoogleHomePage.new @browser
-
-	page.searchBox.when_present.set query
+  page.searchBox.when_present.set query
 end
 
 Then /I should see/ do |text|
